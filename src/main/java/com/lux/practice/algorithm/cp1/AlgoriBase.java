a9186005ba151857b79beb663989e9ee16994e47
luxzhang <hxzhangliang@foxmail.com>
2018-10-24 21:48:46 +0800

    随机连接完成；添加直方图

--- src/main/java/com/lux/practice/algorithm/cp1/AlgoriBase.java
+++ src/main/java/com/lux/practice/algorithm/cp1/AlgoriBase.java
@@ -7,6 +7,9 @@ import org.junit.Test;
 
 import com.lux.practice.algorithm.utils.PrintUtils;
 
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdRandom;
+
 /**
  * @author huanxin
  * 
@@ -311,9 +314,82 @@ public class AlgoriBase {
 		}
 	}
 	
-	@Test
+//	@Test
 	public void pra31() {
-		//TODO 随机连接。从命令行获取整数N及dubbo值p（0到1之间）作为参数，
+		//随机连接。从命令行获取整数N及dubbo值p（0到1之间）作为参数，
 		//	在一个圆上画出大小为0.05且间距相等的N个点，然后将每对点按照概率p用灰线连接。
+		randomLink(20, 0.2);
+		System.out.println("随机连接完成。。。");
+	}
+	
+	/**
+	 * 画圆
+	 * @param x 圆心x坐标
+	 * @param y 圆心y坐标
+	 * @param r 半径r
+	 */
+	private void drawCircle(double x, double y, double r) {
+	    StdDraw.setXscale(0, 2 * x);
+	    StdDraw.setYscale(0, 2 * y);
+	    StdDraw.setPenRadius(0.003);
+	    StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE);
+	    StdDraw.circle(x, y, r);
+	}
+
+	/**
+	 * 在圆上描点
+	 * @param x0 圆心x坐标
+	 * @param y0 圆心y坐标
+	 * @param r 半径r
+	 * @param N N个点
+	 */
+	private double[][] drawPoints(double x0, double y0, double r, int N) {
+	    double[][] points = new double[N][2];
+	    StdDraw.setPenRadius(0.005);
+	    StdDraw.setPenColor(StdDraw.BOOK_RED);
+	    for(int idx = 0; idx < N; ++idx) {
+	        double x = x0 + r * Math.cos(2 * Math.PI * idx / N);
+	        double y = y0 + r * Math.sin(2 * Math.PI * idx / N);
+	        StdDraw.point(x, y);
+	        points[idx][0] = x;
+	        points[idx][1] = y;
+	    }
+	    return points;
+	}
+
+	/**
+	 * 以概率p随机连接顶点集points中的点
+	 * @param points    点集
+	 * @param p 概率p
+	 */
+	private void randomLinkPoints(double[][] points, double p) {
+	    StdDraw.setPenRadius(0.002);
+	    StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
+	    int length = points.length;
+	    for(int i = 0; i < length; ++i)
+	        for(int j = 0; j < length; ++j)
+	            if(true == StdRandom.bernoulli(p))
+	                StdDraw.line(points[i][0], points[i][1], points[j][0], points[j][1]); // 应该再建立一个包含x坐标和y坐标的数据结构
+	}
+
+	/**
+	 * 在圆上画N个点然后每两点间以概率p连接
+	 * @param N N个点
+	 * @param p 概率p
+	 */
+	private void randomLink(int N, double p) {
+	    double x = 10.0;
+	    double y = 10.0;
+	    double r = 9.0;
+	    drawCircle(x, y, r);
+	    double[][] points = drawPoints(x, y, r, N);
+	    randomLinkPoints(points, p);
+	}
+	
+	@Test
+	public void pra32() {
+		//直方图。假设标准输入流中含有一系列double 值。
+		//编写一段程序，从命令行接受一个整数N 和两个double 值l 和r。将(l，r) 分为N 段并使用StdDraw 画出输入流中的值落入每段的数量的直方图。
+		System.out.println("直方图完成。。。");
 	}
 }
